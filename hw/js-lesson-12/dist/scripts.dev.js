7f84bafeae595b139949fb78f12e91b6ce057529
IsakovaL <helis.riba@gmail.com>
2021-01-06 00:44:14 +0200

    why so?

---
+++ hw/js-lesson-12/dist/scripts.dev.js
@@ -0,0 +1,21 @@
+"use strict";
+
+//   $(document).ready(function(){
+//     $('.multiple-items').slick();
+//     infinite: true;
+//     slidesToShow: 4;
+//     slidesToScroll: 4;
+//   });
+//Указываем что будем выводить по 3 слайда на экран
+// $('.multiple-items').slick({
+//   infinite: true,
+//   slidesToShow: 4,
+//   slidesToScroll: 4
+// });
+$(document).ready(function () {
+  $('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4
+  });
+});
---
+++ hw/js-lesson-12/scripts.js
@@ -0,0 +1,30 @@
+
+//   $(document).ready(function(){
+//     $('.multiple-items').slick();
+//     infinite: true;
+
+//     slidesToShow: 4;
+  
+//     slidesToScroll: 4;
+//   });
+
+//Указываем что будем выводить по 3 слайда на экран
+
+// $('.multiple-items').slick({
+
+//   infinite: true,
+
+//   slidesToShow: 4,
+
+//   slidesToScroll: 4
+
+// });
+$(document).ready(function(){
+    $('.multiple-items').slick({
+        infinite: true,
+
+        slidesToShow: 4,
+      
+        slidesToScroll: 4,
+    });
+  });
