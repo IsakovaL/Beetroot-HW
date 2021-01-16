9b9e99260a99c933cd21598fee2e4f0ffcddad76
IsakovaL <helis.riba@gmail.com>
2021-01-16 17:31:24 +0200

    commit-1

---
+++ hw/exam-2/js/dist/script.dev.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// $('.single-item').slick();
+$(document).ready(function () {
+  $('.slick-slider').slick({
+    // infinite: true,
+    // dots:true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 1000
+  });
+});
---
+++ hw/exam-2/js/script.js
@@ -0,0 +1,11 @@
+// $('.single-item').slick();
+$(document).ready(function(){
+    $('.slick-slider').slick({
+        // infinite: true,
+        // dots:true,
+        arrows:false,
+        autoplay:true,
+      
+      autoplaySpeed:1000
+    });
+  });
