ef30c436be5265cd3240ab57129931440d8c6d6b
elena <helis.riba@gmail.com>
2021-02-25 08:13:33 -0800

    map added and fixed

--- hw/exam-3/js/script.js
+++ hw/exam-3/js/script.js
@@ -1,15 +1,15 @@
 // Initialize and add the map
 function initMap() {
     // The location of Uluru
-    const uluru = { lat: 49.994054610112826, lng: 36.22843778462689 };
+    const bursatski = { lat: 49.994054610112826, lng: 36.22843778462689 };
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: uluru,
+      zoom: 14,
+      center: bursatski,
     });
     // The marker, positioned at Uluru
     const marker = new google.maps.Marker({
-      position: uluru,
+      position: bursatski,
       map: map,
     });
   }
